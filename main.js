19e00464c1978e5c0bec1d336937ba5baf734a0e
Josiah Swab <josiahjswab@gmail.com>
2019-09-12 11:51:58 -0700

    instructions removed comments added

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-// Write your JavaScript code here!
+// .----------------.  .----------------.  .----------------. 
+// | .--------------. || .--------------. || .--------------. |
+// | |  _______     | || |      __      | || |  ________    | |
+// | | |_   __ \    | || |     /  \     | || | |_   ___ `.  | |
+// | |   | |__) |   | || |    / /\ \    | || |   | |   `. \ | |
+// | |   |  __ /    | || |   / ____ \   | || |   | |    | | | |
+// | |  _| |  \ \_  | || | _/ /    \ \_ | || |  _| |___.' / | |
+// | | |____| |___| | || ||____|  |____|| || | |________.'  | |
+// | |              | || |              | || |              | |
+// | '--------------' || '--------------' || '--------------' |
+//  '----------------'  '----------------'  '----------------' 
 var planets = [
   ['Pluto', 0.06],
   ['Neptune', 1.148],
@@ -13,40 +23,27 @@ var planets = [
   ['Sun', 27.9]
 ];
 
-planets.forEach(createSelection);// var.forEach(function) this pulles the all the objects from the array making them parameters forEach and exectues the function/parameter
+planets.forEach(createSelection); // var.forEach(function) this pulles the all the objects from the array making them parameters forEach and exectues the function/parameter
 
-function createSelection(item) { // function called above which is also the parent of the above arrays.
-  var option = document.createElement('option'); // brings into exsistence the element option which is attached to the variable option. This is done for clarity. 
-  var dropdownplanets = document.getElementById('planets')
-  dropdownplanets.appendChild(option).textContent = item[0];
+function createSelection(pname) { // function called above which is also the parent of the above arrays
+  var option = document.createElement('option'); // brings into exsistence the element option which is attached to the variable option. This is done for clarity
+  var dropdownplanets = document.getElementById('planets') // pulls the ID planets from the html 
+  dropdownplanets.appendChild(option).innerText = pname[0]; // planet ID is being made the parent of the option element which is binded to the text renderer .innerText = parameter holding planets names and its position in the array [0]
 }
 
 
 
-function calculateWeight(weight, planetName) {
-  // 2. Write the code to return the correct weight
-  var planetName = document.getElementById('planets').selectedIndex;
-  var newGravity = planets[planetName][1];
+function calculateWeight(weight, planetName) { //this function cannot be completed until some variable are defined below. this should run when the button is clicked
+  var planetName = document.getElementById('planets').selectedIndex; //this takes the selected planet from the drop down and assigns it a variable
+  var newGravity = planets[planetName][1]; //this takes the selected planted and runs it through the array to locate its name and then selects integer position 1
 
   return weight * newGravity;
 }
 
-calculateWeight();
-
-function handleClickEvent() {
-  // 3. Create a variable called userWeight and assign the value of the user's weight.
-  var weight = parseFloat(document.getElementById('user-weight').value);
-  // 4. Create a variable called planetName and assign the name of the selected planet from the drop down.
-  var planetName = document.getElementById('planets').value;
-  // 5. Create a variable called result and assign the value of the new calculated weight.
-  var result = calculateWeight(weight, planetName);
-  // 6. Write code to display the message shown in the screenshot
-  document.getElementById('output').innerHTML = ' If you were on ' + planetName + ', you would weigh ' + result + 'lbs';
-  document.body.innerHTML = document.body.innerHTML.replace('NaN', '0'); 
-  //before adding this code above if you did not input a numerical value into the input it would return NaN now it returns 0
+function handleClickEvent() {//this function is fired when button is clicked
+  var weight = parseFloat(document.getElementById('user-weight').value);// var assigned to the value input by the user
+  var planetName = document.getElementById('planets').value;// var assigned to the value of the planet selected by user
+  var result = calculateWeight(weight, planetName);// the above function given a new name to keep code clear and clean.
+  document.getElementById('output').innerHTML = ' If you were on ' + planetName + ', you would weigh ' + result + 'lbs';//innerHTML is writing the following string to the ID output in the html file
+  document.body.innerHTML = document.body.innerHTML.replace('NaN', '0');//before adding this code above if you had not input a numerical value the input would return NaN now it returns 0
 }
-// 7. Set the #calculate-button element's onclick method to use the handleClickEvent function.
-
-// Bonus Challenges
-// 8. Reverse the drop down order so that the sun is first.
-// 9. Make it look nice using bootstrap (http://getbootstrap.com/getting-started/)
